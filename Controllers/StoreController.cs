4e3471bf32a0a91c24dc7c5c198d1646343ec812
Robert Nordgren <info@robertnordgren.se>
2019-02-27 16:03:54 +0100

    Fixed remove ingredient function, Half working Confirm Order function

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -2,21 +2,30 @@
 using Inlämning_2___Webshop.Models;
 using Inlämning_2___Webshop.ViewModels;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Inlämning_2___Webshop.Controllers
 {
     public class StoreController : Controller
     {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
         private TomasosContext _tomasosContext;
         private ApplicationDBContext _applicationContext;
         public const string sessionName = "cart";
 
-        public StoreController(TomasosContext tomasosContext, ApplicationDBContext applicationContext)
+        public StoreController(TomasosContext tomasosContext, ApplicationDBContext applicationContext, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+
             _tomasosContext = tomasosContext;
             _applicationContext = applicationContext;
         }
@@ -25,10 +34,6 @@ namespace Inlämning_2___Webshop.Controllers
         {
             CheckCart();
             var model = _tomasosContext.Matratt.ToList();
-            //var model = new MenuIngredientViewModel();
-            //model.Recipes = _tomasosContext.Matratt.ToList();
-            //model.Ingredients = _tomasosContext.Produkt.ToList();
-            //model.RecipeIngredients = _tomasosContext.MatrattProdukt.ToList();
 
             return View(model);
         }
@@ -84,6 +89,71 @@ namespace Inlämning_2___Webshop.Controllers
             return PartialView("_Cart", cart);
         }
 
+        public async Task<IActionResult> ConfirmOrder()
+        {
+            List<Matratt> cart;
+            var currentUser = await _userManager.GetUserAsync(HttpContext.User);
 
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(sessionName)))
+            {
+                return RedirectToAction("Menu", "Store");
+            }
+            else
+            {
+                var amount = 0;
+
+                var temp = HttpContext.Session.GetString(sessionName);
+                cart = JsonConvert.DeserializeObject<List<Matratt>>(temp);
+
+                var order = new Bestallning
+                {
+                    KundId = currentUser.Id,
+                    BestallningDatum = DateTime.Now,
+                    Totalbelopp = cart.Sum(x => x.Pris),
+                    Levererad = false
+                };
+
+                _tomasosContext.Bestallning.Add(order);
+
+                foreach (var item in cart.Distinct())
+                {
+                    var bestallningMatratt = new BestallningMatratt
+                    {
+                        //BestallningId = order.BestallningId,
+                        Bestallning = order,
+                        Antal = cart.Where(x => x.MatrattId == item.MatrattId).Count(),
+                        //MatrattId = item.MatrattId
+                        Matratt = item
+                    };
+
+                    _tomasosContext.BestallningMatratt.Add(bestallningMatratt);
+                    bestallningMatratt = null;
+                }
+
+                _tomasosContext.SaveChanges();
+                _tomasosContext.Dispose();
+
+                //var order = new BestallningMatratt
+                //{
+
+                //    Antal = 1,
+                //    MatrattId = cart[0].MatrattId
+                //};
+
+
+                //foreach (var item in cart)
+                //{
+                //    var order = new Bestallning
+                //    {
+                //        BestallningDatum = DateTime.Now,
+                //        Totalbelopp = cart.Sum(x => x.Pris),
+                //        KundId = currentUser.Id,
+                //        Levererad = false
+                //    };
+                //}
+            }
+
+            return View();
+        }
     }
 }
---
+++ ViewModels/ConfirmOrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Inlämning_2___Webshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inlämning_2___Webshop.ViewModels
+{
+    public class ConfirmOrderViewModel
+    {
+        List<Bestallning> Orders { get; set; }
+    }
+}
